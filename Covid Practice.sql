2b7dc4e5d4cb9f6a4da0c320a92c73b4eedf01aa
pmoua001 <pmoua8906@gmail.com>
2025-06-02 20:37:39 -0500

    Update Covid Practice.sql

--- Covid Practice.sql
+++ Covid Practice.sql
@@ -1,12 +1,17 @@
-
-Select 
-	YEAR(date) AS year_of,
-	state,
-	SUM(cases) AS case_per_state,
-	SUM(deaths) AS deaths_per_state
-FROM covid
+-Finding the amount of cases and deaths per state
+SELECT
+	covid.state AS states_affected,
+	SUM(cases) AS cases_per_state,
+	SUM(deaths) AS deaths_per_state,
+	us_population._2020_census AS population_num
+FROM 
+	covid INNER JOIN us_pop_by_state AS us_population
+	ON covid.state = us_population.state
+--Only wanted the 50 states, covid table had 55 states listed
 GROUP BY
-	date, state
-ORDER BY
-	year_of
-	
+	covid.state, us_population._2020_census
+--Grouped by state total numbers of deaths and cases 
+--Grouped by census to help group other data
+ORDER BY 
+	cases_per_state DESC, deaths_per_state DESC
+--Finding the highest case and deaths by state
